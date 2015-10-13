fb71799e79fff9e1063298552dcaa78e89a9f522
Léo Millon <millon.leo@gmail.com>
2015-10-13 22:05:23 +0200

    Simple maven project

---
+++ src/main/java/com/github/leomillon/converter/App.java
@@ -0,0 +1,13 @@
+package com.github.leomillon.converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
